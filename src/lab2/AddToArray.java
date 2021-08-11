3ecb5fbd0727d89a8aeb4087262d9ded50c4fa75
glenchiridza <glenchiridz@gmail.com>
2021-08-11 10:46:07 +0200

    addto array

--- src/lab2/AddToArray.java
+++ src/lab2/AddToArray.java
@@ -4,4 +4,17 @@ package lab2;
  * Created by glenc on Aug 2021
  **/
 public class AddToArray {
+
+    public static void main(String[] args) {
+        double[] arr = new double[]{3.5,1.8,4.2,1.4};
+        double total = 0;
+        System.out.println("total sum of array elements");
+        for (double item : arr){
+            total += item;
+        }
+        System.out.println(total);
+
+
+
+    }
 }
