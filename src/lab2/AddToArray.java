4d264bb19bd033ec38b5a95d9f1563845dfad63e
glenchiridza <glenchiridz@gmail.com>
2021-08-11 11:07:34 +0200

    sum-largest-smallest

--- src/lab2/AddToArray.java
+++ src/lab2/AddToArray.java
@@ -1,5 +1,7 @@
 package lab2;
 
+import java.util.Formatter;
+
 /**
  * Created by glenc on Aug 2021
  **/
@@ -7,13 +9,30 @@ public class AddToArray {
 
     public static void main(String[] args) {
         double[] arr = new double[]{3.5,1.8,4.2,1.4};
-        double total = 0;
-        System.out.println("total sum of array elements");
+        double sum = 0;
+        double largest = arr[0];
+        double smallest = arr[arr.length-1];
+        StringBuilder elem= new StringBuilder();
         for (double item : arr){
-            total += item;
-        }
-        System.out.println(total);
+            //list of the array elements
+            elem.append(item).append(", ");
 
+            //sum up
+            sum += item;
+
+            //find largest and smallest
+            if (item > largest)
+                largest = item;
+            else if (item < smallest)
+                smallest = item;
+
+        }
+
+        System.out.println("Elements in array are "+elem);
+        System.out.print("total sum \t Largest \t Smallest\n");
+        Formatter f = new Formatter();
+        f.format("%7.1f %9.1f %12.1f",sum,largest,smallest);
+        System.out.println(f);
 
 
     }
