2e8e43159a24051d200a78da363423ec3627ffaf
glenchiridza <glenchiridz@gmail.com>
2021-08-30 15:26:44 +0200

    exponentiation

---
+++ src/lab3/Exponential.java
@@ -0,0 +1,20 @@
+package lab3;
+
+/**
+ * Created by glenc on Aug 2021
+ **/
+public class Exponential {
+
+    public static void main(String[] args) {
+        int base = 3;
+        int exponent = 4;
+        int result = 1;
+
+        while (exponent !=0){
+            result *=base;
+            --exponent;
+        }
+
+        System.out.format("%d to the power %d = %d",base,exponent,result);
+    }
+}
