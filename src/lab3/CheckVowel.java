b31a80df4d1fdeed239d90830708a5d5054fa1b7
glenchiridza <glenchiridz@gmail.com>
2021-08-30 14:02:00 +0200

    character input

---
+++ src/lab3/CheckVowel.java
@@ -0,0 +1,21 @@
+package lab3;
+
+import java.util.Scanner;
+
+/**
+ * Created by glenc on Aug 2021
+ **/
+public class CheckVowel {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please input a character to check if its either vowel or consonants");
+        System.out.println(" ");
+
+        Scanner input = new Scanner(System.in);
+        char i = input.next().charAt(0);
+
+
+    }
+
+}
