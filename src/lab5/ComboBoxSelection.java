8e9593b2bbade3169acc8fdc4f40370f2465eeec
glenchiridza <glenchiridz@gmail.com>
2021-09-02 21:01:00 +0200

    combo box done

--- src/lab5/ComboBoxSelection.java
+++ src/lab5/ComboBoxSelection.java
@@ -2,6 +2,8 @@ package lab5;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 /**
  * Created by glenc on Sep 2021
@@ -21,6 +23,28 @@ public class ComboBoxSelection extends JFrame {
         setLayout(new FlowLayout());
 
         listCombo = new JComboBox(programs);
+        listCombo.setMaximumRowCount(3);
 
+        listCombo.addItemListener(
+                new ItemListener() {
+                    @Override
+                    public void itemStateChanged(ItemEvent itemEvent) {
+                        if (itemEvent.getStateChange() == ItemEvent.SELECTED){
+                            label.setText(programs[listCombo.getSelectedIndex()]);
+                        }
+                    }
+                }
+        );
+        add(listCombo);
+        label = new JLabel(programs[0]);
+        add(label);
+    }
+
+    public static void main(String[] args) {
+        ComboBoxSelection listCombo = new ComboBoxSelection();
+        listCombo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        listCombo.setSize(350, 350);
+        listCombo.setVisible(true);
     }
 }
+
